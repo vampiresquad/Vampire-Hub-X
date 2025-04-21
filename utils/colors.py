082909416b4db58afb4614392818e427728b8d6b
Vampire Squad <vampiresquad.org@gmail.com>
2025-04-21 17:04:05 +0600

    Create colors.py

---
+++ utils/colors.py
@@ -0,0 +1,33 @@
+# utils/colors.py
+
+# Text Colors
+red     = '\033[91m'
+green   = '\033[92m'
+yellow  = '\033[93m'
+blue    = '\033[94m'
+magenta = '\033[95m'
+cyan    = '\033[96m'
+white   = '\033[97m'
+black   = '\033[90m'
+
+# Bold Text Colors
+bold_red     = '\033[1;91m'
+bold_green   = '\033[1;92m'
+bold_yellow  = '\033[1;93m'
+bold_blue    = '\033[1;94m'
+bold_magenta = '\033[1;95m'
+bold_cyan    = '\033[1;96m'
+bold_white   = '\033[1;97m'
+
+# Background Colors
+bg_red     = '\033[41m'
+bg_green   = '\033[42m'
+bg_yellow  = '\033[43m'
+bg_blue    = '\033[44m'
+bg_magenta = '\033[45m'
+bg_cyan    = '\033[46m'
+bg_white   = '\033[47m'
+bg_black   = '\033[40m'
+
+# Reset Code
+reset = '\033[0m'
