83db64728f3b813aeec0d625e910ffc9f70c8802
Vampire Squad <vampiresquad.org@gmail.com>
2025-04-21 12:57:55 +0600

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,1 +1,25 @@
+# core/banner.py
 
+from utils.colors import *
+
+def admin_banner():
+    print(f"""{red}
+ █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█████
+ ████▒▒{bright_white}     VAMPIRE-ADMIN-ACCESS ACTIVE     {red}▒▒████
+ ███▒▒{cyan}      Coded by: Muhammad Shourov (VAMPIRE)   {red}▒▒███
+ ██▒▒{yellow}   Founder of Vampire Squad | GitHub: vampiresquad  {red}▒▒██
+ ██▒▒{yellow}   Email: vampiresquad.org@gmail.com         {red}▒▒██
+ █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+{reset}""")
+
+
+def user_banner():
+    print(f"""{blue}
+ █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█████
+ ████▒▒{bright_white}       FREE-USER-ACCESS ACTIVE       {blue}▒▒████
+ ███▒▒{cyan}  Coded by: Muhammad Shourov (VAMPIRE)        {blue}▒▒███
+ ██▒▒{yellow}  GitHub: github.com/vampiresquad              {blue}▒▒██
+ ██▒▒{yellow}  Facebook: fb.com/Junior.Writer.SHourov       {blue}▒▒██
+ ██▒▒{yellow}  Email: vampiresquad.org@gmail.com            {blue}▒▒██
+ █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+{reset}""")
